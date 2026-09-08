c868e2154bab267849c433ab91d094c40018a9e9
sathwikaranga229-ui <sathwikaranga229@gmail.com>
2026-09-08 12:26:43 +0530

    Intial Commit

--- src/ParkingSlot.java
+++ src/ParkingSlot.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 public class ParkingSlot {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int n = 1000;
-        System.out.println("Available parking slots=5");
-        for(int i=0;i<=n;i++){
-            System.out.println("Enter parking slot number:  ");
-        int slot=sc.nextInt();
-            if(slot>=1 && slot<=5){
-                System.out.println("SlotAvailable");
-            }
-            else{
-                System.out.println("NotAvailable");
+        System.out.println("Enter Number of Slots: ");
+        int n = sc.nextInt();
+
+        for (int i = 0;i<=n;i++){
+            System.out.println("Enter Slot Number: ");
+            int slot = sc.nextInt();
+            if (slot<=n){
+                System.out.println("Slots available");
+            }else{
+                System.out.println("Not available");
             }
         }
     }
